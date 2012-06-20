2480f04b0d06aa81533033a9e386bc66ae4d0cca
Yagi <straitwalk@gmail.com>
2012-06-20 21:01:31 +0900

    skypeのチャット名を取得するメソッド追加

--- skype_client.rb
+++ skype_client.rb
@@ -5,6 +5,20 @@ include SkypeMac
 
 class SkypeClient < BaseClient
   def send_message(message)
-    Skype.send_(command: "CHATMESSAGE #{@config['chat_id']} #{message}")
+    send_command "CHATMESSAGE #{@config['chat_id']} #{message}"
+  end
+
+  def chat_ids
+    send_command("SEARCH RECENTCHATS").sub("CHATS ","").split(/,/)
+  end
+
+  def room_name(chat_id)
+    name_ary = send_command("GET CHAT #{chat_id} FRIENDLYNAME").scan(/.*FRIENDLYNAME\s(.+)$/)
+    name_ary.empty? ? "" : name_ary[0][0]
+  end
+
+  private
+  def send_command(command)
+    Skype.send_(command: command)
   end
 end
